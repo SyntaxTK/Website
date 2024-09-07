f1a08ce5dd452cf4175605a41029ff0a76840c9a
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-09-07 11:16:03 +0200

    Deleted standard vite and react images, commented out code that use the deleted images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,14 +8,16 @@ function App() {
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </div> */}    
+      {/* <link rel="icon"  href="public/logo/syntax logo - light mode - clear.png" /> */}
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
